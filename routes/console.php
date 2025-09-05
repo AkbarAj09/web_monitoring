98f60406b48d4aa96aea2922f3b7aba3fe342c46
AkbarAj09 <210210502010@student.unm.ac.id>
2025-09-05 23:49:49 +0700

    buat routing

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+use App\Http\Controllers\BackController;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::call(function () {
+    app(BackController::class)->getDataRahasiaBisnis();
+})->everyMinute()->name('getDataRahasiaBisnis');
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BackController;
+use App\Http\Controllers\FrontController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [FrontController::class, 'index'])->name('home');
+Route::post('/login', [BackController::class, 'login'])->name('login');
+Route::get('/register', [FrontController::class, 'register'])->name('register');
+Route::post('/register-simpan', [BackController::class, 'registerStore'])->name('register.store');
+
+Route::get('/get-data-rahasia-bisnis-kuesioner', [BackController::class, 'getDataRahasiaBisnis']);
+
+Route::get('/logout', [FrontController::class, 'logout'])->name('logout');
+
+Route::middleware(['auth', 'checkrole:Admin'])->group(function () {
+    Route::get('/admin/home', [FrontController::class, 'homeAdmin'])->name('admin.home');
 });
