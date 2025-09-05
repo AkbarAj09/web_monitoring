c9c41c07af1dd2945ceca4cb20f1af221fb737a4
AkbarAj09 <210210502010@student.unm.ac.id>
2025-09-05 23:48:08 +0700

    buat halaman homepage & auth

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,101 @@
+@extends('master')
+@section('title') Dashboard @endsection
+@section('css')
+
+<style>
+    .btn-ref {
+        position: fixed;
+        top: 50px;
+        left: 1000px;
+    }
+
+    #loading-overlay {
+
+        position: fixed;
+
+        top: 0;
+
+        left: 0;
+
+        width: 100%;
+
+        height: 100%;
+
+        background: rgba(0, 0, 0, 0.7);
+
+        z-index: 9999;
+
+        display: none;
+
+    }
+
+    .table {
+        background-color: #f9f9f9;
+        border-radius: 8px;
+        overflow: hidden;
+        width: 100%;
+        max-width: 100%;
+        margin-top: 15px;
+        border: 0.5px solid #ccc;
+        table-layout: auto;
+
+        /* Allow dynamic column sizing */
+    }
+
+    .table th,
+    .table td {
+        padding: 8px !important;
+        font-size: 16px;
+        border: 0.5px solid #ccc;
+        color: #313131;
+        text-align: center;
+    }
+
+    .table th {
+        font-weight: bold;
+        text-align: center;
+        /* Align text to the left */
+    }
+
+
+    .table tbody tr {
+        transition: background-color 0.3s;
+    }
+
+    .table tbody tr:hover {
+        background-color: #e2e2e2;
+    }
+
+    .table tbody tr:nth-child(odd) {
+        background-color: #f2f2f2;
+        /* Odd row background color */
+    }
+
+    .table tbody tr:nth-child(even) {
+        background-color: #ffffff;
+        /* Even row background color */
+    }
+
+    @media (max-width: 768px) {
+
+        .table th,
+        .table td {
+            font-size: 12px;
+        }
+    }
+</style>
+@endsection
+
+@section('content')
+@if(session('success'))
+<div class="alert alert-success">
+    {{ session('success') }}
+</div>
+@endif
+
+@if(session('error'))
+<div class="alert alert-danger">
+    {{ session('error') }}
+</div>
+@endif
+@endsection
