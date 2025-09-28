278c19ee118fd22c576cabab1e8fba087e781054
AkbarAj09 <210210502010@student.unm.ac.id>
2025-09-28 18:38:50 +0700

    update summary

--- routes/console.php
+++ routes/console.php
@@ -3,29 +3,33 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
-use App\Http\Controllers\BackController;
+use App\Http\Controllers\FrontController;
+use App\Http\Controllers\GetDataController;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
+Schedule::call(function () {
+    app(FrontController::class)->refreshSummarySimpatiTiktok();
+})->everyMinute()->name('refreshSummarySimpatiTiktok');
 
 Schedule::call(function () {
-    app(BackController::class)->getDataCreatorPartner();
+    app(GetDataController::class)->getDataCreatorPartner();
 })->everyMinute()->name('getDataCreatorPartner');
 
 Schedule::call(function () {
-    app(BackController::class)->getDataPadiUmkm();
+    app(GetDataController::class)->getDataPadiUmkm();
 })->everyMinute()->name('getDataPadiUmkm');
 
 Schedule::call(function () {
-    app(BackController::class)->getDataReferralChampionAm();
+    app(GetDataController::class)->getDataReferralChampionAm();
 })->everyMinute()->name('getDataReferralChampionAm');
 
 Schedule::call(function () {
-    app(BackController::class)->getDataSimpatiTiktok();
+    app(GetDataController::class)->getDataSimpatiTiktok();
 })->everyMinute()->name('getDataSimpatiTiktok');
 
 Schedule::call(function () {
-    app(BackController::class)->getDataSultamRacing();
+    app(GetDataController::class)->getDataSultamRacing();
 })->everyMinute()->name('getDataSultamRacing');
