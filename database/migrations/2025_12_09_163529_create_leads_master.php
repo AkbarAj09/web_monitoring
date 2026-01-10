7bc3ef9b76a00672ff3c3ab01dace57b75dcfb1f
AkbarAj09 <210210502010@student.unm.ac.id>
2026-01-10 22:44:13 +0700

    fix: add table exists check in leads_master migration

--- database/migrations/2025_12_09_163529_create_leads_master.php
+++ database/migrations/2025_12_09_163529_create_leads_master.php
@@ -11,26 +11,28 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('leads_master', function (Blueprint $table) {
-            $table->id();
-            $table->integer('user_id');          // Handled by Canvasser
-            $table->integer('source_id')->nullable();          // Source
-            $table->integer('sector_id');          // Sector
-            $table->string('regional')->nullable();          // Regional
-            $table->string('kode_voucher')->nullable();        // Kode Voucher
-            $table->string('company_name');        // Company Name
-            $table->string('mobile_phone');        // Mobile Phone
-            $table->string('email')->nullable();   // Email
-            $table->integer('status')->default(0); // cek (ex: "OK")
-            $table->string('nama')->nullable();    // Nama
-            $table->text('address')->nullable();  // Address
-            $table->string('myads_account')->nullable();   // Akun MyAds kalau ada
-            $table->string('data_type');   // Data Type
-            $table->decimal('komitmen', 5, 2)->default(0);   // Data Type
-            $table->integer('plan_min_topup')->default(0);   // Data Type
-            $table->text('remarks')->nullable();   // Remarks
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('leads_master')) {
+            Schema::create('leads_master', function (Blueprint $table) {
+                $table->id();
+                $table->integer('user_id');          // Handled by Canvasser
+                $table->integer('source_id')->nullable();          // Source
+                $table->integer('sector_id');          // Sector
+                $table->string('regional')->nullable();          // Regional
+                $table->string('kode_voucher')->nullable();        // Kode Voucher
+                $table->string('company_name');        // Company Name
+                $table->string('mobile_phone');        // Mobile Phone
+                $table->string('email')->nullable();   // Email
+                $table->integer('status')->default(0); // cek (ex: "OK")
+                $table->string('nama')->nullable();    // Nama
+                $table->text('address')->nullable();  // Address
+                $table->string('myads_account')->nullable();   // Akun MyAds kalau ada
+                $table->string('data_type');   // Data Type
+                $table->decimal('komitmen', 5, 2)->default(0);   // Data Type
+                $table->integer('plan_min_topup')->default(0);   // Data Type
+                $table->text('remarks')->nullable();   // Remarks
+                $table->timestamps();
+            });
+        }
     }
 
     /**
